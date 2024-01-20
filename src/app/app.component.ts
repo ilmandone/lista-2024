6dae6dfa16d044a1dabead22b85d6e913b65a35b
Andrea Mandini <a.mandini@mauti.it>
2024-01-20 19:42:57 +0100

    feat: install primeNG with scss reset and ripple animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { PrimeNGConfig} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  private _primeNGConfig = inject(PrimeNGConfig)
+
   title = 'lista-2024';
+
+  ngOnInit() {
+    this._primeNGConfig.ripple = true
+  }
 }
