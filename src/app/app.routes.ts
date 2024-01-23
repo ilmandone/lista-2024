8ce39b0eac83295b83b75c3193079b7790398767
Andrea Mandini <a.mandini@mauti.it>
2024-01-23 23:14:33 +0100

    feat: setup login and home pages component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login.page/login.page.component').then(m => m.LoginPageComponent)
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home.page/home.page.component').then(m => m.HomePageComponent)
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: "full"
+  }
+];
