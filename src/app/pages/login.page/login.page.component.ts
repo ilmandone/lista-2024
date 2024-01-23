1182defc71c522a61c22138afd00c408bf3d8615
Andrea Mandini <a.mandini@mauti.it>
2024-01-23 23:20:04 +0100

    feat (login): setup hero image

--- src/app/pages/login.page/login.page.component.ts
+++ src/app/pages/login.page/login.page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-login.page',
   standalone: true,
-  imports: [],
+    imports: [
+        NgOptimizedImage
+    ],
   templateUrl: './login.page.component.html',
   styleUrl: './login.page.component.scss'
 })
