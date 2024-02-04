3d7d42073fe3e391e34a5a77af9fd987ab1153a3
Andrea Mandini <a.mandini@mauti.it>
2024-02-04 18:56:43 +0100

    feat: logout from side menu

--- src/app/pages/home.page/home.page.component.ts
+++ src/app/pages/home.page/home.page.component.ts
@@ -4,13 +4,13 @@ import {ButtonModule} from 'primeng/button';
 import {RippleModule} from 'primeng/ripple';
 import {FirebaseAuthentication} from '../../services/firebase/authe.service';
 import {DbService} from 'app/services/firebase/db.service';
-import {SidebarModule} from "primeng/sidebar";
 import {SideMenuAction, SideMenuComponent} from "../../components/side-menu/side-menu.component";
+import {LoaderComponent} from "../../components/loader/loader.component";
 
 @Component({
 	selector: 'app-home.page',
 	standalone: true,
-	imports: [ButtonModule, RippleModule, RouterModule, SideMenuComponent],
+	imports: [ButtonModule, RippleModule, RouterModule, SideMenuComponent, LoaderComponent],
 	templateUrl: './home.page.component.html',
 	styleUrl: './home.page.component.scss',
 })
@@ -18,6 +18,7 @@ export class HomePageComponent implements OnInit {
 	private _authSrv = inject(FirebaseAuthentication);
 	private _dbSrv = inject(DbService);
 
+	loading = false
 	mainMenuOpen = false
 
 	ngOnInit() {
@@ -28,7 +29,10 @@ export class HomePageComponent implements OnInit {
 	sideMenuAction($event: SideMenuAction) {
 		switch($event) {
 			case 'logout':
+				this.loading = true
 				this._authSrv.logout()
+					.then(_ => { this.loading = false})
+					.catch(_ => { this.loading = false })
 				break
 		}
 	}
