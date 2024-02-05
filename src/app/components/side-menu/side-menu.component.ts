817ccde11be526586140e9b932060ba72ff63ec4
Andrea Mandini <andrea.mandini@ditechspa.it>
2024-02-05 13:43:51 +0100

    feat (side-menu): theme switch

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,18 +1,27 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {SidebarModule} from "primeng/sidebar";
-import {ButtonModule} from "primeng/button";
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { SidebarModule } from 'primeng/sidebar';
+import { ButtonModule } from 'primeng/button';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
-export type SideMenuAction = 'logout' | 'reload' | 'groups'
+export type SideMenuAction = 'logout' | 'reload' | 'groups';
 
 @Component({
 	selector: 'app-side-menu',
 	standalone: true,
-	imports: [SidebarModule, ButtonModule],
+	imports: [
+		SidebarModule,
+		ButtonModule,
+		InputSwitchModule,
+		ReactiveFormsModule,
+	],
 	templateUrl: './side-menu.component.html',
-	styleUrl: './side-menu.component.scss'
+	styleUrl: './side-menu.component.scss',
 })
 export class SideMenuComponent {
-	@Input() visible!: boolean
-	@Output() visibleChange = new EventEmitter<boolean>()
-	@Output() action = new EventEmitter<SideMenuAction>()
+	@Input() visible!: boolean;
+	@Output() visibleChange = new EventEmitter<boolean>();
+	@Output() action = new EventEmitter<SideMenuAction>();
+
+	checkedFC = new FormControl(false);
 }
