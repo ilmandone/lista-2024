8a115264df02e32bf58ac5f1828cf67808eb38a4
Andrea Mandini <andrea.mandini@ditechspa.it>
2024-02-05 13:54:06 +0100

    feat (theme): add theme as signal

--- src/app/services/_common/theme.service.ts
+++ src/app/services/_common/theme.service.ts
@@ -1,36 +1,44 @@
-import {inject, Injectable} from '@angular/core';
-import {DOCUMENT} from "@angular/common";
+import { inject, Injectable, signal } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 export enum Theme {
-  LIGHT = 'light',
-  DARK = 'dark'
+	LIGHT = 'light',
+	DARK = 'dark',
 }
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class ThemeService {
-  private _document = inject(DOCUMENT)
-  private _current : Theme = Theme.LIGHT
+	private _document = inject(DOCUMENT);
+	private _current = signal<Theme>(Theme.LIGHT);
 
-  constructor() {}
+	constructor() {}
 
-  /**
-   * Set specific theme and save status in _current
-   * @param {Theme} theme
-   */
-  setTheme(theme: Theme) {
-    const themeLinkTag = this._document.getElementById('app-theme') as HTMLLinkElement
-    if(themeLinkTag) {
-      themeLinkTag.href = `${theme}.css`
-      this._current = theme
-    }
-  }
+	get theme() {
+		return this._current;
+	}
 
-  /**
-   * Toggle theme
-   */
-  toggleTheme() {
-    this.setTheme(this._current === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
-  }
+	/**
+	 * Set specific theme and save status in _current
+	 * @param {Theme} theme
+	 */
+	setTheme(theme: Theme) {
+		const themeLinkTag = this._document.getElementById(
+			'app-theme',
+		) as HTMLLinkElement;
+		if (themeLinkTag) {
+			themeLinkTag.href = `${theme}.css`;
+			this._current.set(theme);
+		}
+	}
+
+	/**
+	 * Toggle theme
+	 */
+	toggleTheme() {
+		this.setTheme(
+			this._current() === Theme.LIGHT ? Theme.DARK : Theme.LIGHT,
+		);
+	}
 }
