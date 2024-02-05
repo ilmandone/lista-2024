2699a8286d3d678a18beff2468c8361b67b72f3b
Andrea Mandini <andrea.mandini@ditechspa.it>
2024-02-05 13:54:30 +0100

    feat (side-menu): switch theme behaviour

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,8 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+	Component,
+	EventEmitter,
+	Input,
+	OnInit,
+	Output,
+	effect,
+	inject,
+} from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Theme, ThemeService } from 'app/services/_common/theme.service';
 
 export type SideMenuAction = 'logout' | 'reload' | 'groups';
 
@@ -18,10 +27,18 @@ export type SideMenuAction = 'logout' | 'reload' | 'groups';
 	templateUrl: './side-menu.component.html',
 	styleUrl: './side-menu.component.scss',
 })
-export class SideMenuComponent {
+export class SideMenuComponent implements OnInit {
 	@Input() visible!: boolean;
 	@Output() visibleChange = new EventEmitter<boolean>();
 	@Output() action = new EventEmitter<SideMenuAction>();
 
+	private _themeSrv = inject(ThemeService);
+
 	checkedFC = new FormControl(false);
+
+	ngOnInit(): void {
+		this.checkedFC.valueChanges.subscribe((r) => {
+			this._themeSrv.setTheme(r ? Theme.DARK : Theme.LIGHT);
+		});
+	}
 }
--- src/app/pages/home.page/home.page.component.ts
+++ src/app/pages/home.page/home.page.component.ts
@@ -1,16 +1,25 @@
-import {Component, inject, OnInit} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {ButtonModule} from 'primeng/button';
-import {RippleModule} from 'primeng/ripple';
-import {FirebaseAuthentication} from '../../services/firebase/authe.service';
-import {DbService} from 'app/services/firebase/db.service';
-import {SideMenuAction, SideMenuComponent} from "../../components/side-menu/side-menu.component";
-import {LoaderComponent} from "../../components/loader/loader.component";
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { FirebaseAuthentication } from '../../services/firebase/authe.service';
+import { DbService } from 'app/services/firebase/db.service';
+import {
+	SideMenuAction,
+	SideMenuComponent,
+} from '../../components/side-menu/side-menu.component';
+import { LoaderComponent } from '../../components/loader/loader.component';
 
 @Component({
 	selector: 'app-home.page',
 	standalone: true,
-	imports: [ButtonModule, RippleModule, RouterModule, SideMenuComponent, LoaderComponent],
+	imports: [
+		ButtonModule,
+		RippleModule,
+		RouterModule,
+		SideMenuComponent,
+		LoaderComponent,
+	],
 	templateUrl: './home.page.component.html',
 	styleUrl: './home.page.component.scss',
 })
@@ -18,8 +27,8 @@ export class HomePageComponent implements OnInit {
 	private _authSrv = inject(FirebaseAuthentication);
 	private _dbSrv = inject(DbService);
 
-	loading = false
-	mainMenuOpen = false
+	loading = false;
+	mainMenuOpen = false;
 
 	ngOnInit() {
 		/*this._dbSrv.init();
@@ -27,13 +36,18 @@ export class HomePageComponent implements OnInit {
 	}
 
 	sideMenuAction($event: SideMenuAction) {
-		switch($event) {
+		switch ($event) {
 			case 'logout':
-				this.loading = true
-				this._authSrv.logout()
-					.then(_ => { this.loading = false})
-					.catch(_ => { this.loading = false })
-				break
+				this.loading = true;
+				this._authSrv
+					.logout()
+					.then((_) => {
+						this.loading = false;
+					})
+					.catch((_) => {
+						this.loading = false;
+					});
+				break;
 		}
 	}
 }
