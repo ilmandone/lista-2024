355c5dd757d2a4c8e9ec7e7ffc08edc7a1e7e1f4
Andrea Mandini <andrea.mandini@ditechspa.it>
2024-02-08 18:25:27 +0100

    feat (dialog-new): add action buttons and expose events to parent

--- src/app/components/dialog-new/dialog-new.component.ts
+++ src/app/components/dialog-new/dialog-new.component.ts
@@ -1,15 +1,36 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
+
+export enum DialogNewActionType {
+	OK,
+	CANCEL,
+	ADD,
+}
+export type DialogNewAction = DialogNewActionType;
 
 @Component({
 	selector: 'app-dialog-new',
 	standalone: true,
-	imports: [DialogModule],
+	imports: [DialogModule, ButtonModule],
 	templateUrl: './dialog-new.component.html',
 	styleUrl: './dialog-new.component.scss',
 })
 export class DialogNewComponent {
 	@Input() visible: boolean = false;
+	@Input() header!: string;
+	@Input() showAdd = true;
 
 	@Output() visibleChange = new EventEmitter<boolean>();
+	@Output() action = new EventEmitter<DialogNewAction>();
+
+	dialogNewActionType = DialogNewActionType;
+
+	closeDialog(action: DialogNewActionType) {
+		this.action.emit(action);
+		if (action !== DialogNewActionType.ADD) {
+			this.visible = false;
+			this.visibleChange.emit(this.visible);
+		}
+	}
 }
