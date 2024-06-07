2c564ffb965200327b58e626e7cad18a4ce29e4e
Andrea Mandini <a.mandini@mauti.it>
2024-06-07 18:58:41 +0200

    feat: add dark and light themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from "@angular/material/button";
 
@@ -9,6 +9,14 @@ import {MatButtonModule} from "@angular/material/button";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'lista-2024';
+
+  ngOnInit() {
+    const cssEl = document.getElementById('app-theme') as HTMLLinkElement
+    setTimeout(() => {
+        cssEl.href = 'theme-dark.css'
+    },5000)
+
+  }
 }
