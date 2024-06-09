80faa5f53855877544d6244399e0f5e994a3bdc7
Andrea Mandini <a.mandini@mauti.it>
2024-06-09 16:19:16 +0200

    feat: add home component as root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,12 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/login/login.component').then(c => c.LoginComponent),
   },
   {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.component').then(c => c.HomeComponent),
+  },
+  {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/home',
     pathMatch: 'full',
   }
 ];
