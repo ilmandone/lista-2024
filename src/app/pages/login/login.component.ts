e4dfd1faf22bf057769aaea0a2e3b303adca2a32
Andrea Mandini <a.mandini@mauti.it>
2024-06-09 18:25:50 +0200

    feat(login): inputs with errors

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {Nullable} from "../../shared/utils";
 
@@ -14,18 +14,21 @@ interface ILoginFG {
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage, MatButton, ReactiveFormsModule, MatFormField, MatInput, MatLabel],
+  imports: [CommonModule, NgOptimizedImage, MatButton, ReactiveFormsModule, MatFormField, MatInput, MatLabel, MatError],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  loginFormGroup= new FormGroup<ILoginFG>({
-    email: new FormControl(null, [Validators.required]),
-    password: new FormControl(null, [Validators.required]),
+  emailFC = new FormControl(null, [Validators.required])
+  passwordFC = new FormControl(null, [Validators.required])
+
+  loginFG= new FormGroup<ILoginFG>({
+    email: this.emailFC,
+    password: this.passwordFC
   })
 
   loginSubmit() {
-    console.log(this.loginFormGroup.value)
+    console.log(this.loginFG.value)
   }
 }
