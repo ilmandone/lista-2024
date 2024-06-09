d8ce958fad5749ebc7959b6726348a45c4e28b41
Andrea Mandini <a.mandini@mauti.it>
2024-06-09 18:04:46 +0200

    feat(login): setup form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import {CommonModule, NgOptimizedImage} from "@angular/common";
+import {MatButton} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {Nullable} from "../../shared/utils";
+
+interface ILoginFG {
+  email: FormControl<Nullable<string>>
+  password: FormControl<Nullable<string>>
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, NgOptimizedImage],
+  imports: [CommonModule, NgOptimizedImage, MatButton, ReactiveFormsModule, MatFormField, MatInput, MatLabel],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginFormGroup= new FormGroup<ILoginFG>({
+    email: new FormControl(null, [Validators.required]),
+    password: new FormControl(null, [Validators.required]),
+  })
+
+  loginSubmit() {
+    console.log(this.loginFormGroup.value)
+  }
 }
