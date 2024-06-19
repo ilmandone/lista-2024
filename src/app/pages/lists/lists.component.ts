177657cd344d6f800fc29b950bc4520942ef628e
Andrea Manini <a.mandini@vargroup.it>
2024-06-19 18:03:02 +0200

    feat (lists): header with title and control

--- src/app/pages/lists/lists.component.ts
+++ src/app/pages/lists/lists.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule],
   templateUrl: './lists.component.html',
-  styleUrl: './lists.component.scss'
+  styleUrl: './lists.component.scss',
 })
-export class ListsComponent {
-
-}
+export class ListsComponent {}
