e1a8719a21001f1d5399199ad7ad7257cf420e46
Andrea Manini <a.mandini@vargroup.it>
2024-06-19 14:24:47 +0200

    feat: start with main common layout

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,26 +1,32 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterOutlet } from "@angular/router";
-import { FirebaseService } from "../../shared/firebase.service";
+import { Router, RouterOutlet } from '@angular/router';
+import { FirebaseService } from '../../shared/firebase.service';
 
 import { CommonModule } from '@angular/common';
-import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatSidenavModule, CommonModule, RouterOutlet],
+  imports: [
+    MatSidenavModule,
+    CommonModule,
+    RouterOutlet,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss',  
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
-
   private _fbSrv = inject(FirebaseService);
-  private _router = inject(Router)
+  private _router = inject(Router);
 
   logOut() {
-
     this._fbSrv.logout().then(() => {
-      void this._router.navigate(['/login'])
-    })
+      void this._router.navigate(['/login']);
+    });
   }
 }
