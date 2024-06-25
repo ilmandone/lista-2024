b273fe8814120feef7097839bd170b754d62f66e
Andrea Manini <a.mandini@vargroup.it>
2024-06-25 18:41:07 +0200

    feat: loading lists - start

--- src/app/pages/lists/lists.component.ts
+++ src/app/pages/lists/lists.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, OnInit, inject } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { FirebaseService } from 'app/shared/firebase.service'
 
 @Component({
   selector: 'app-lists',
   standalone: true,
   imports: [MatIconModule, MatButtonModule],
   templateUrl: './lists.component.html',
-  styleUrl: './lists.component.scss',
+  styleUrl: './lists.component.scss'
 })
-export class ListsComponent {}
+export class ListsComponent implements OnInit {
+
+  private _firebaseSrv = inject(FirebaseService)
+  ngOnInit(): void {
+    this._firebaseSrv.startDB()
+
+    this._firebaseSrv.loadLists().then(r => console.log(r))
+  }
+}
--- src/app/shared/firebase.service.ts
+++ src/app/shared/firebase.service.ts
@@ -1,28 +1,30 @@
-import { Injectable, signal } from '@angular/core';
-import { FirebaseApp, FirebaseOptions, initializeApp } from 'firebase/app';
+import { Injectable, signal } from '@angular/core'
+import { FirebaseApp, FirebaseOptions, initializeApp } from 'firebase/app'
 import {
   Auth,
   getAuth,
   sendPasswordResetEmail,
   signInWithEmailAndPassword,
   signOut,
-  UserCredential,
-} from 'firebase/auth';
+  UserCredential
+} from 'firebase/auth'
 
-import { environment } from 'environments/environment.development';
+import { Firestore, getFirestore, getDoc, doc } from 'firebase/firestore'
+
+import { environment } from 'environments/environment.development'
 
 export interface IIsLogged {
-  state: boolean | null;
-  error?: string;
+  state: boolean | null
+  error?: string
 }
 
-export type IResetPsw = IIsLogged;
+export type IResetPsw = IIsLogged
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class FirebaseService {
-  private _env = environment as { [key: string]: string };
+  private _env = environment as Record<string, string>
   private _fireBaseOptions: FirebaseOptions = {
     authDomain: this._env['AUTH_DOMAIN'],
     apiKey: this._env['API_KEY'],
@@ -30,17 +32,18 @@ export class FirebaseService {
     projectId: this._env['PROJECT_ID'],
     storageBucket: this._env['STORAGE_BUCKET'],
     messagingSenderId: this._env['MESSAGING_SENDER_ID'],
-    appId: this._env['APP_ID'],
-  };
+    appId: this._env['APP_ID']
+  }
 
-  private _app!: FirebaseApp;
-  private _auth!: Auth;
-  private _userData!: UserCredential;
+  private _app!: FirebaseApp
+  private _auth!: Auth
+  private _db!: Firestore
+  private _userData!: UserCredential
 
-  public isLogged = signal<IIsLogged>({ state: false });
+  public isLogged = signal<IIsLogged>({ state: false })
 
   get userData() {
-    return this._userData;
+    return this._userData
   }
 
   /**
@@ -48,16 +51,18 @@ export class FirebaseService {
    */
   public start(): Promise<void> {
     return new Promise((resolve) => {
-      this._app = initializeApp(this._fireBaseOptions);
-      this._auth = getAuth(this._app);
+      this._app = initializeApp(this._fireBaseOptions)
+      this._auth = getAuth(this._app)
 
       // Listen logout / login
       this._auth.onAuthStateChanged((event) => {
-        this.isLogged.set({ state: event != null });
-        resolve();
-      });
-    });
+        this.isLogged.set({ state: event != null })
+        resolve()
+      })
+    })
   }
+
+  //#region Authentication
 
   /**
    * Login with email and password
@@ -68,14 +73,14 @@ export class FirebaseService {
   login(email: string, password: string): Promise<void> {
     return signInWithEmailAndPassword(this._auth, email, password)
       .then((resp) => {
-        this._userData = resp;
-        this.isLogged.set({ state: true });
-        return;
+        this._userData = resp
+        this.isLogged.set({ state: true })
+        return
       })
       .catch((error) => {
-        this.isLogged.set({ state: false, error: error.code });
-        throw new Error(error);
-      });
+        this.isLogged.set({ state: false, error: error.code })
+        throw new Error(error)
+      })
   }
 
   /**
@@ -85,20 +90,45 @@ export class FirebaseService {
   logout(): Promise<void> {
     return signOut(this._auth)
       .then(() => {
-        this.isLogged.set({ state: false });
-        return;
+        this.isLogged.set({ state: false })
+        return
       })
       .catch((error) => {
-        throw new Error(error);
-      });    
+        throw new Error(error)
+      })
   }
 
   /**
    * Reset account with email
-   * @param {string} email 
+   * @param {string} email
    * @returns {Promise<void>}
    */
   resetWithPassword(email: string): Promise<void> {
-    return sendPasswordResetEmail(this._auth, email);
+    return sendPasswordResetEmail(this._auth, email)
   }
+
+  //#endregion
+
+  //#region DB
+
+  private async _getDoc() {
+    return await getDoc(doc(this._db, 'ListaDellaSpesaV2', 'ListaDellaSpesaV2'))
+  }
+
+  startDB() {
+    if (!this._app) throw new Error('App not initialized')
+    this._db = getFirestore(this._app)
+  }
+
+  async loadLists() {
+    try {
+      const doc = await this._getDoc()
+      if (doc.exists()) return doc.data()
+      else throw new Error('Doc not found')
+    } catch (error) {
+      throw new Error(error as string)
+    }
+  }
+
+  //#endregion
 }
