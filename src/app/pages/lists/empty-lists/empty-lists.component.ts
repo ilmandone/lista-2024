1145319902fc99ab50d9bdc49ca82d8ce85b6b4b
Andrea Manini <a.mandini@vargroup.it>
2024-06-27 18:12:26 +0200

    feat: empty list component

---
+++ src/app/pages/lists/empty-lists/empty-lists.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-empty-lists',
+  standalone: true,
+  imports: [],
+  templateUrl: './empty-lists.component.html',
+  styleUrl: './empty-lists.component.scss'
+})
+export class EmptyListsComponent {
+
+}
