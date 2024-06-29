48e0dfd06cdbcb1c6e016f1e1cf0427d68a84a30
Andrea Mandini <a.mandini@mauti.it>
2024-06-29 16:32:35 +0200

    feat: list item

---
+++ src/app/pages/lists/lists.item/lists.item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lists.item',
+  standalone: true,
+  imports: [],
+  templateUrl: './lists.item.component.html',
+  styleUrl: './lists.item.component.scss'
+})
+export class ListsItemComponent {
+
+}
