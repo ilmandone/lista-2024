eef03fa1362d6660800de04c9e66737faf41d50e
Andrea Mandini <a.mandini@mauti.it>
2024-07-02 22:11:29 +0200

    feat(lists.item): double clicks and styles

--- src/app/pages/lists/lists.item/lists.item.component.ts
+++ src/app/pages/lists/lists.item/lists.item.component.ts
@@ -1,15 +1,20 @@
 import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core'
 import { ListData } from '../../../data/firebase.interfaces'
+import { MatRippleModule } from '@angular/material/core'
 
 @Component({
   selector: 'app-lists-item',
   standalone: true,
-  imports: [],
+  imports: [MatRippleModule],
   templateUrl: './lists.item.component.html',
   styleUrl: './lists.item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListsItemComponent implements OnInit{
+
+  static readonly DOUBLE_TAP_TIME = 400
+  private lastTapTS = new Date().getTime()
+
   data = input.required<ListData>()
 
   time!: Date
@@ -18,7 +23,14 @@ export class ListsItemComponent implements OnInit{
     this.time = new Date(this.data().updated.seconds)
   }
 
-  clicked() {
-    console.log('clicked')
+  tapped() {
+    const tapTime = new Date().getTime()
+    const tapLength = tapTime - this.lastTapTS
+
+    if (tapLength < ListsItemComponent.DOUBLE_TAP_TIME && tapLength > 0) {
+      console.log('DOUBLE_TAP_TIME: ', tapLength)
+    }
+
+    this.lastTapTS = tapTime
   }
 }
