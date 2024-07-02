cd5312837a7897fa9de976e6c1e3e172f019834b
Andrea Mandini <a.mandini@mauti.it>
2024-07-02 23:11:24 +0200

    feat(lists.item): start editing mode

--- src/app/pages/lists/lists.item/lists.item.component.ts
+++ src/app/pages/lists/lists.item/lists.item.component.ts
@@ -1,21 +1,26 @@
-import { ChangeDetectionStrategy, Component, input, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, ElementRef, HostBinding, input, OnInit, ViewChild } from '@angular/core'
 import { ListData } from '../../../data/firebase.interfaces'
 import { MatRippleModule } from '@angular/material/core'
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 @Component({
   selector: 'app-lists-item',
   standalone: true,
-  imports: [MatRippleModule],
+  imports: [MatRippleModule, MatInputModule, MatFormFieldModule],
   templateUrl: './lists.item.component.html',
   styleUrl: './lists.item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListsItemComponent implements OnInit{
+  data = input.required<ListData>()
+  @HostBinding('class.editing') isEditing = false
+  @ViewChild('input', {static: true}) inputEl!: ElementRef<HTMLInputElement>
 
   static readonly DOUBLE_TAP_TIME = 400
-  private lastTapTS = new Date().getTime()
+  static readonly FOCUS_DELAY = 100
 
-  data = input.required<ListData>()
+  private lastTapTS = new Date().getTime()
 
   time!: Date
 
@@ -24,13 +29,23 @@ export class ListsItemComponent implements OnInit{
   }
 
   tapped() {
+    if(this.isEditing) return
+
     const tapTime = new Date().getTime()
     const tapLength = tapTime - this.lastTapTS
 
     if (tapLength < ListsItemComponent.DOUBLE_TAP_TIME && tapLength > 0) {
-      console.log('DOUBLE_TAP_TIME: ', tapLength)
+      this.isEditing = true
+
+      window.setTimeout(() => {
+        this.inputEl.nativeElement.focus()
+      },ListsItemComponent.FOCUS_DELAY)
     }
 
     this.lastTapTS = tapTime
+  }
+
+  overlayHit() {
+    this.isEditing = false
   }
 }
