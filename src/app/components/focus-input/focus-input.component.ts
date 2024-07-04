39e322659250fb61c16d9842437fcc3e363d5681
Andrea Manini <a.mandini@vargroup.it>
2024-07-04 08:58:20 +0200

    feat(focus-input): add overlay

--- src/app/components/focus-input/focus-input.component.ts
+++ src/app/components/focus-input/focus-input.component.ts
@@ -1,15 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-focus-input',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule],
   templateUrl: './focus-input.component.html',
-  styleUrl: './focus-input.component.scss'
+  styleUrl: './focus-input.component.scss'  
 })
 export class FocusInputComponent {
   value = input<string | number>()
   disabled = input<boolean>(false)
 
+  focused = output<boolean>()
+
   editing = false
+
+  onFocus() {
+    this.editing = true
+    this.focused.emit(true)
+  }
+
+  onBlur() {
+    this.editing = false
+    this.focused.emit(false)
+  }
 }
