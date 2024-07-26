19d58d3b3158dbcd12fedcd325d1bfb30d9cb21f
Andrea Mandini <a.mandini@mauti.it>
2024-07-26 21:57:54 +0200

    feat(confirm-cancel): confirm button size

--- src/app/components/confirm-cancel/confirm-cancel.component.ts
+++ src/app/components/confirm-cancel/confirm-cancel.component.ts
@@ -1,19 +1,21 @@
 import { Component, effect, HostBinding, inject, input, output } from '@angular/core'
 import { FocusInputService } from '../focus-input/focus-input.service'
 import { MatButtonModule } from '@angular/material/button'
+import { NgStyle } from '@angular/common'
 
 @Component({
   selector: 'app-confirm-cancel',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, NgStyle],
   templateUrl: './confirm-cancel.component.html',
   styleUrl: './confirm-cancel.component.scss'
 })
 export class ConfirmCancelComponent {
   private _focusInput = inject(FocusInputService)
 
-  confirmLabel = input<string>()
   cancelLabel = input<string>()
+  confirmLabel = input<string>()
+  confirmSize = input<number>()
 
   cancel = output()
   confirm = output()
