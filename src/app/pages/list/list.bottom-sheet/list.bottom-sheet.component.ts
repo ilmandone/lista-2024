1630d3f3916cf80ff07339be2d996adb1adee228
Andrea Mandini <a.mandini@mauti.it>
2024-07-26 15:28:16 +0200

    feat(list.bottom-sheet): add component

---
+++ src/app/pages/list/list.bottom-sheet/list.bottom-sheet.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-bottom-sheet',
+  standalone: true,
+  imports: [],
+  templateUrl: './list.bottom-sheet.component.html',
+  styleUrl: './list.bottom-sheet.component.scss'
+})
+export class ListBottomSheetComponent {
+
+}
