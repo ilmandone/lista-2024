a47593dc5000425c63266a581966fe2cb7a96443
Andrea Mandini <a.mandini@mauti.it>
2024-07-29 22:10:38 +0200

    feat(lists.item): start

--- src/app/pages/list/list.item/list.item.component.ts
+++ src/app/pages/list/list.item/list.item.component.ts
@@ -1,14 +1,29 @@
-import { Component, input } from '@angular/core'
+import { Component, inject, input, signal } from '@angular/core'
 import { ItemData } from '../../../data/firebase.interfaces'
+import { MatRippleModule } from '@angular/material/core'
+import { FocusInputComponent } from '../../../components/focus-input/focus-input.component'
+import { FocusInputService } from '../../../components/focus-input/focus-input.service'
+import { Nullable } from '../../../shared/common.interfaces'
 
 @Component({
   selector: 'app-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    MatRippleModule,
+    FocusInputComponent
+  ],
   templateUrl: './list.item.component.html',
   styleUrl: './list.item.component.scss'
 })
 export class ListItemComponent {
-  data = input.required<ItemData>()
+  readonly focusSrv = inject(FocusInputService)
 
+  data = input.required<ItemData>()
+  editing = signal<boolean>(false)
+
+  disabled = false
+
+  itemLabelChanged($event: Nullable<string>) {
+    console.log($event)
+  }
 }
