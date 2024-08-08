1669d765b7fd7160e73ad888d74fdb8446dd03c2
Andrea Mandini <a.mandini@vargroup.it>
2024-08-08 21:46:35 +0200

    feat(firebase.interface): restore toBuy and inCart for item change

--- src/app/data/firebase.interfaces.ts
+++ src/app/data/firebase.interfaces.ts
@@ -2,7 +2,7 @@ import { Timestamp } from 'firebase/firestore'
 
 export interface BasicItemChange {
 	UUID: string,
-  crud?: 'create' | 'update' | 'delete',
+  	crud?: 'create' | 'update' | 'delete',
 }
 
 export interface ListData extends Pick<BasicItemChange, 'UUID'> {
@@ -14,16 +14,18 @@ export interface ListData extends Pick<BasicItemChange, 'UUID'> {
 export type ListsData = ListData[]
 export type ListsItemChanges = Omit<ListData, 'updated'> & Pick<BasicItemChange, 'crud'>
 
-export interface ItemData extends Pick<BasicItemChange, 'UUID'> {
+export interface ItemsChanges extends BasicItemChange {
 	inCart: boolean
 	label: string
 	toBuy: boolean
 	group: string
 	position: number
-}
+	[key:string]: unknown // ???
+} 
 
+export type ItemData = Exclude<ItemsChanges, 'crud'>
 export type ItemsData = ItemData[]
-export type ItemsChanges = Omit<ItemData, 'toBuy' | 'inCart'> & Pick<BasicItemChange, 'crud'>
+// export type ItemsChanges = Omit<ItemData, 'toBuy' | 'inCart'> & Pick<BasicItemChange, 'crud'>
 
 export interface ItemsChangesEditBag<T> {
   created: T[]
