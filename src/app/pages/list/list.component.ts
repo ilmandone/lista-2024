f2865475da9b5c9d4d41d3690f7478b45b58b788
Andrea Mandini <a.mandini@mauti.it>
2024-08-09 16:38:03 +0200

    perf: save inCart items id in a set and use it for reset

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -7,8 +7,8 @@ import { ItemData, ItemsChanges, ItemsData } from '../../data/firebase.interface
 import { LoaderComponent } from '../../components/loader/loader.component'
 import { MatBottomSheet, MatBottomSheetModule } from '@angular/material/bottom-sheet'
 import {
-	IListBottomSheetData,
-	ListBottomSheetComponent
+  IListBottomSheetData,
+  ListBottomSheetComponent
 } from './list.bottom-sheet/list.bottom-sheet.component'
 import { ButtonToggleComponent } from '../../components/button-toggle/button-toggle.component'
 import { ConfirmCancelComponent } from '../../components/confirm-cancel/confirm-cancel.component'
@@ -19,61 +19,63 @@ import { ListItemSelectedEvent } from './list.item/list.item.interface'
 import { CdkDrag, CdkDragDrop, CdkDragPlaceholder, CdkDropList } from '@angular/cdk/drag-drop'
 import { SetOfItemsChanges } from 'app/data/items.changes'
 import { MainStateService } from '../../shared/main-state.service'
-import { DeleteConfirmDialogComponent } from '../../shared/delete.confirm.dialog/delete.confirm.dialog.component'
+import {
+  DeleteConfirmDialogComponent
+} from '../../shared/delete.confirm.dialog/delete.confirm.dialog.component'
 import { addItem, deleteItem, updateItemAttr, updateItemPosition } from './list.cud'
 import { cloneDeep } from 'lodash'
 
 @Component({
-	selector: 'app-list',
-	standalone: true,
-	imports: [
-		MatIcon,
-		MatIconButton,
-		LoaderComponent,
-		MatBottomSheetModule,
-		ButtonToggleComponent,
-		ConfirmCancelComponent,
-		ListItemComponent,
-		MatDialogModule,
-		CdkDrag,
-		CdkDropList,
-		CdkDragPlaceholder
-	],
-	templateUrl: './list.component.html',
-	styleUrl: './list.component.scss'
+  selector: 'app-list',
+  standalone: true,
+  imports: [
+    MatIcon,
+    MatIconButton,
+    LoaderComponent,
+    MatBottomSheetModule,
+    ButtonToggleComponent,
+    ConfirmCancelComponent,
+    ListItemComponent,
+    MatDialogModule,
+    CdkDrag,
+    CdkDropList,
+    CdkDragPlaceholder
+  ],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss'
 })
 class ListComponent implements OnInit {
 
   private readonly AUTOSAVE_TIME_OUT = 1200
 
-	private readonly _activatedRoute = inject(ActivatedRoute)
-	private readonly _bottomSheet = inject(MatBottomSheet)
-	private readonly _dialog = inject(MatDialog)
-	private readonly _firebaseSrv = inject(FirebaseService)
-	private readonly _mainStateSrv = inject(MainStateService)
-	private _UUID!: string
-	private _itemsChanges = new SetOfItemsChanges<ItemsChanges>()
-	private _itemsDataCache: ItemData[] = []
-  private _autoSaveTimeOutID!:  number
+  private readonly _activatedRoute = inject(ActivatedRoute)
+  private readonly _bottomSheet = inject(MatBottomSheet)
+  private readonly _dialog = inject(MatDialog)
+  private readonly _firebaseSrv = inject(FirebaseService)
+  private readonly _mainStateSrv = inject(MainStateService)
+  private _UUID!: string
+  private _itemsChanges = new SetOfItemsChanges<ItemsChanges>()
+  private _itemsDataCache: ItemData[] = []
+  private _autoSaveTimeOutID!: number
   private _inCartItems = new Set<number>()
 
-	editing = false
-	itemsData = signal<ItemData[]>([])
-	label!: string
+  editing = false
+  itemsData = signal<ItemData[]>([])
+  label!: string
   selectedItems = new Set<string>()
-	shopping = false
-	viewModeGrid = false
+  shopping = false
+  viewModeGrid = false
 
-	async ngOnInit() {
-		this._UUID = this._activatedRoute.snapshot.params['id']
-		this.label = this._activatedRoute.snapshot.params['label']
+  async ngOnInit() {
+    this._UUID = this._activatedRoute.snapshot.params['id']
+    this.label = this._activatedRoute.snapshot.params['label']
 
-		this._firebaseSrv.loadList(this._UUID).then((r) => {
-			this.itemsData.set(r)
-		})
-	}
+    this._firebaseSrv.loadList(this._UUID).then((r) => {
+      this.itemsData.set(r)
+    })
+  }
 
-	//#region Editing
+  //#region Editing
 
   /**
    * Handle autosave for not to buy data update
@@ -83,11 +85,11 @@ class ListComponent implements OnInit {
     this._autoSaveTimeOutID = window.setTimeout(this._saveItems.bind(this), this.AUTOSAVE_TIME_OUT)
   }
 
-	/**
-	 * Update items
-	 * @description Save items in db and reset all the edit information
-	 */
-	_saveItems() {
+  /**
+   * Update items
+   * @description Save items in db and reset all the edit information
+   */
+  _saveItems() {
     this._mainStateSrv.showLoader()
     this._firebaseSrv.updateList(this._itemsChanges.values, this._UUID).then((r) => {
       this.itemsData.set(r)
@@ -99,111 +101,121 @@ class ListComponent implements OnInit {
       this._mainStateSrv.hideLoader()
       this.editing = false
     })
-	}
+  }
 
-	/**
-	 * Add an item
-	 * @description If another item is selected the new one is added after it
-	 * @param {string} label
-	 */
-	addItem(label: string) {
-		const selectedUUID =
-			this.selectedItems.size > 0 ? this.selectedItems.values().next().value : null
-		const insertAfter = selectedUUID
-			? this.itemsData().find((e) => e.UUID === selectedUUID)?.position ??
-				this.itemsData().length - 1
-			: this.itemsData().length - 1
+  /**
+   * Add an item
+   * @description If another item is selected the new one is added after it
+   * @param {string} label
+   */
+  addItem(label: string) {
+    const selectedUUID =
+      this.selectedItems.size > 0 ? this.selectedItems.values().next().value : null
+    const insertAfter = selectedUUID
+      ? this.itemsData().find((e) => e.UUID === selectedUUID)?.position ??
+      this.itemsData().length - 1
+      : this.itemsData().length - 1
 
-		const { itemsData, changes } = addItem(label, this.itemsData() as ItemsData, insertAfter)
-
-		this.itemsData.set(itemsData)
-		this._itemsChanges.set(changes)
-
-		this.selectedItems.clear()
-	}
-
-	/**
-	 * Delete button click
-	 */
-	itemsDeleted() {
-		const { itemsData, changes } = deleteItem([...this.selectedItems], this.itemsData())
-		this.itemsData.set(itemsData)
-		this._itemsChanges.set(changes)
-	}
-
-	/**
-	 * Item attribute changed
-	 * @param $event
-	 */
-	itemChanged($event: ItemsChanges) {
-		const { itemsData, changes } = updateItemAttr($event, this.itemsData())
+    const { itemsData, changes } = addItem(label, this.itemsData() as ItemsData, insertAfter)
 
     this.itemsData.set(itemsData)
-		this._itemsChanges.set(changes)
-	}
+    this._itemsChanges.set(changes)
 
-	/**
-	 * On item drop update the order
-	 * @param $event
-	 */
-	itemDrop($event: CdkDragDrop<ItemsData>) {
-		const { itemsData, changes } = updateItemPosition($event, this.itemsData())
+    this.selectedItems.clear()
+  }
 
-		this.itemsData.set(itemsData)
-		this._itemsChanges.set(changes)
-	}
+  /**
+   * Delete button click
+   */
+  itemsDeleted() {
+    const { itemsData, changes } = deleteItem([...this.selectedItems], this.itemsData())
+    this.itemsData.set(itemsData)
+    this._itemsChanges.set(changes)
+  }
 
-	/**
-	 * Add or remove and item from the selectedItems set
-	 * @param {ListItemSelectedEvent} $event
-	 */
-	itemSelected($event: ListItemSelectedEvent) {
-		if ($event.isSelected) this.selectedItems.add($event.UUID)
-		else this.selectedItems.delete($event.UUID)
-	}
+  /**
+   * Item attribute changed
+   * @param $event
+   */
+  itemChanged($event: ItemsChanges) {
+    const { itemsData, changes } = updateItemAttr($event, this.itemsData())
 
-	/**
-	 * Open the new item dialog
-	 * @description On confirm the item is added to f/e data
-	 */
-	openNewItemDialog() {
-		const d = this._dialog.open(ListNewDialogComponent)
+    this.itemsData.set(itemsData)
+    this._itemsChanges.set(changes)
+  }
 
-		d.afterClosed().subscribe((r: string) => {
-			if (r) {
-				this.addItem(r)
-			}
-		})
-	}
+  /**
+   * On item drop update the order
+   * @param $event
+   */
+  itemDrop($event: CdkDragDrop<ItemsData>) {
+    const { itemsData, changes } = updateItemPosition($event, this.itemsData())
 
-	//#endregion
+    this.itemsData.set(itemsData)
+    this._itemsChanges.set(changes)
+  }
 
-	//#region Bottom sheet
+  /**
+   * Add or remove and item from the selectedItems set
+   * @param {ListItemSelectedEvent} $event
+   */
+  itemSelected($event: ListItemSelectedEvent) {
+    if ($event.isSelected) this.selectedItems.add($event.UUID)
+    else this.selectedItems.delete($event.UUID)
+  }
 
-	/**
-	 * Open the button sheet and subscribe to the dismiss event
-	 * @description If editing is enable save items to cache
-	 */
-	openButtonSheet() {
-		const p = this._bottomSheet.open(ListBottomSheetComponent, {
-			data: {
-				viewModeGrid: this.viewModeGrid
-			} as IListBottomSheetData
-		})
+  /**
+   * Open the new item dialog
+   * @description On confirm the item is added to f/e data
+   */
+  openNewItemDialog() {
+    const d = this._dialog.open(ListNewDialogComponent)
 
-		p.afterDismissed().subscribe((r: IListBottomSheetData) => {
-			Object.assign(this, { ...r })
-			if (this.editing) this._itemsDataCache = cloneDeep(this.itemsData())
-		})
-	}
+    d.afterClosed().subscribe((r: string) => {
+      if (r) {
+        this.addItem(r)
+      }
+    })
+  }
 
-	//#endregion
+  //#endregion
 
-	//#region Interaction
+  //#region Bottom sheet
 
-  private _resetInCart(data: ItemsData, inCartItems:Set<number>): {newItemsData: ItemsData, changes: ItemsChanges[]} {
+  /**
+   * Open the button sheet and subscribe to the dismiss event
+   * @description If editing is enable save items to cache
+   */
+  openButtonSheet() {
+    const p = this._bottomSheet.open(ListBottomSheetComponent, {
+      data: {
+        viewModeGrid: this.viewModeGrid
+      } as IListBottomSheetData
+    })
+
+    p.afterDismissed().subscribe((r: IListBottomSheetData) => {
+      Object.assign(this, { ...r })
+      if (this.editing) this._itemsDataCache = cloneDeep(this.itemsData())
+    })
+  }
+
+  //#endregion
+
+  //#region Interaction
+
+  /**
+   * Reset in cart state for all in cart items
+   * @description return changes with new items data
+   * @param {ItemsData} data
+   * @param {Set<number>} inCartItems
+   * @private
+   */
+  private _resetInCart(data: ItemsData, inCartItems: Set<number>): {
+    newItemsData: ItemsData,
+    changes: ItemsChanges[]
+  } {
     const newItemsData = cloneDeep(data)
-    const changes: ItemsChanges[] =[]
+    const changes: ItemsChanges[] = []
 
     inCartItems.forEach(index => {
       const d = data[index]
@@ -219,9 +231,19 @@ class ListComponent implements OnInit {
     return { newItemsData, changes }
   }
 
-  private _fromInCartToNotToBuy(data: ItemsData, inCartItems:Set<number>): {newItemsData: ItemsData, changes: ItemsChanges[]} {
+  /**
+   * Set items from inCart to notToBuy
+   * @description return changes with new items data
+   * @param {itemsData} data
+   * @param {Set<number>} inCartItems
+   * @private
+   */
+  private _fromInCartToNotToBuy(data: ItemsData, inCartItems: Set<number>): {
+    newItemsData: ItemsData,
+    changes: ItemsChanges[]
+  } {
     const newItemsData = cloneDeep(data)
-    const changes: ItemsChanges[] =[]
+    const changes: ItemsChanges[] = []
 
     inCartItems.forEach(index => {
       const d = newItemsData[index]
@@ -243,7 +265,7 @@ class ListComponent implements OnInit {
    * @description Update the item with the $event data for notToBuy and inCart properties
    * @param {ItemsChanges} $event
    */
-	itemClicked($event: ItemsChanges) {
+  itemClicked($event: ItemsChanges) {
     if (!this.editing) {
 
       if (this.shopping) {
@@ -259,68 +281,70 @@ class ListComponent implements OnInit {
       this.itemChanged($event)
       this._engageSaveItems()
     }
-	}
+  }
 
-	//#endregion
+  //#endregion
 
-	//#region Confirm / Cancel
+  //#region Confirm / Cancel
 
-	/**
-	 * Confirm shopping or editing mode
-	 */
-	confirm() {
-		if (this.shopping) {
+  /**
+   * Confirm shopping or editing mode
+   */
+  confirm() {
+    if (this.shopping) {
 
-      const {newItemsData, changes} = this._fromInCartToNotToBuy(this.itemsData(), this._inCartItems)
+      const {
+        newItemsData,
+        changes
+      } = this._fromInCartToNotToBuy(this.itemsData(), this._inCartItems)
       this._inCartItems.clear()
       this._itemsChanges.set(changes)
       this.itemsData.set(newItemsData)
 
-			this.shopping = false
-
+      this.shopping = false
 
 
       this._engageSaveItems()
 
-		} else {
+    } else {
 
-			if (this._itemsChanges.hasDeletedItems) {
-				const dr = this._dialog.open(DeleteConfirmDialogComponent)
-				dr.afterClosed().subscribe((result) => {
-					if (result) this._saveItems()
-				})
-			} else this._saveItems()
-		}
-	}
+      if (this._itemsChanges.hasDeletedItems) {
+        const dr = this._dialog.open(DeleteConfirmDialogComponent)
+        dr.afterClosed().subscribe((result) => {
+          if (result) this._saveItems()
+        })
+      } else this._saveItems()
+    }
+  }
 
-	/**
-	 * Cancel pressed in shopping or editing mode
-	 * @description Cancel shopping will remove all the in cart status from items. Cancel editing
-	 * will restore the cached data
-	 */
-	cancel() {
-		if (this.shopping) {
+  /**
+   * Cancel pressed in shopping or editing mode
+   * @description Cancel shopping will remove all the in cart status from items. Cancel editing
+   * will restore the cached data
+   */
+  cancel() {
+    if (this.shopping) {
 
-      const {newItemsData, changes} = this._resetInCart(this.itemsData(), this._inCartItems)
+      const { newItemsData, changes } = this._resetInCart(this.itemsData(), this._inCartItems)
       this._inCartItems.clear()
       this._itemsChanges.set(changes)
       this.itemsData.set(newItemsData)
 
-			this.shopping = false
+      this.shopping = false
       this._engageSaveItems()
 
-		} else {
+    } else {
 
       this.itemsData.set(this._itemsDataCache)
       this._itemsDataCache = []
       this.selectedItems.clear()
       this._itemsChanges.clear()
 
-			this.editing = false
-		}
-	}
+      this.editing = false
+    }
+  }
 
-	//#endregion
+  //#endregion
 }
 
 export default ListComponent
