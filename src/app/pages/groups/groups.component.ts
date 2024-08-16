cd3db0f03f7c4780e8914485ba4703b82118f7d3
Andrea Mandini <a.mandini@vargroup.it>
2024-08-16 16:00:52 +0200

    feat(groups): header

--- src/app/pages/groups/groups.component.ts
+++ src/app/pages/groups/groups.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { FocusInputService } from 'app/components/focus-input/focus-input.service';
+import { Location } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-groups',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,MatButtonModule],
   templateUrl: './groups.component.html',
   styleUrl: './groups.component.scss'
 })
 class GroupsComponent {
+  private readonly _focusSrv = inject(FocusInputService)
+  private readonly _location = inject(Location)
+
+  selectedGroups = new Set<string>()
+  disabled = false
+
+  constructor() {
+    effect(() => {
+      this.disabled = this._focusSrv.id() !== null
+    })
+  }
+
+  goBack() {
+    this._location.back()
+  }
 
 }
 
