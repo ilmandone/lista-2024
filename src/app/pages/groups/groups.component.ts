3e634777099d1974000100a5db37c968b217b4e8
Andrea Mandini <a.mandini@vargroup.it>
2024-08-16 17:25:39 +0200

    feat(groups): load groups with page preloader

--- src/app/pages/groups/groups.component.ts
+++ src/app/pages/groups/groups.component.ts
@@ -1,33 +1,53 @@
-import { Component, effect, inject } from '@angular/core';
-import { FocusInputService } from 'app/components/focus-input/focus-input.service';
-import { Location } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { Location } from '@angular/common'
+import { Component, effect, inject, OnInit, signal } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { FocusInputService } from 'app/components/focus-input/focus-input.service'
+import { LoaderComponent } from 'app/components/loader/loader.component'
+import { GroupsData } from 'app/data/firebase.interfaces'
+import { FirebaseService } from 'app/data/firebase.service'
+import { MainStateService } from 'app/shared/main-state.service'
 
 @Component({
-  selector: 'app-groups',
-  standalone: true,
-  imports: [MatIconModule,MatButtonModule],
-  templateUrl: './groups.component.html',
-  styleUrl: './groups.component.scss'
+	selector: 'app-groups',
+	standalone: true,
+	imports: [MatIconModule, MatButtonModule, LoaderComponent],
+	templateUrl: './groups.component.html',
+	styleUrl: './groups.component.scss'
 })
-class GroupsComponent {
-  private readonly _focusSrv = inject(FocusInputService)
-  private readonly _location = inject(Location)
+class GroupsComponent implements OnInit {
+	private readonly _firebaseSrv = inject(FirebaseService)
+	private readonly _focusSrv = inject(FocusInputService)
+	private readonly _location = inject(Location)
+  private readonly _mainStateSrv = inject(MainStateService)
 
-  selectedGroups = new Set<string>()
-  disabled = false
+	selectedGroups = new Set<string>()
+	disabled = false
 
-  constructor() {
-    effect(() => {
-      this.disabled = this._focusSrv.id() !== null
-    })
-  }
+	groups = signal<GroupsData>([])
 
-  goBack() {
-    this._location.back()
-  }
+	constructor() {
+		effect(() => {
+			this.disabled = this._focusSrv.id() !== null
+		})
 
+    effect(
+      async () => {
+        if (this._mainStateSrv.reload()) {
+          this.groups.set(await this._firebaseSrv.loadGroups())
+        }
+      },
+      { allowSignalWrites: true }
+    )
+	}
+
+	async ngOnInit() {
+		this.groups.set(await this._firebaseSrv.loadGroups())
+	}
+
+	goBack() {
+		this._location.back()
+	}
 }
 
 export default GroupsComponent
