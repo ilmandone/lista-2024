3d8f9451ae4ed2149d605c11eb59782927c07dbc
Andrea Mandini <a.mandini@mauti.it>
2024-08-20 18:40:53 +0200

    feat(group): label change

--- src/app/components/group/group.component.ts
+++ src/app/components/group/group.component.ts
@@ -1,11 +1,12 @@
 import { Component, input, output } from '@angular/core'
-import { GroupData } from 'app/data/firebase.interfaces'
+import { GroupChanges, GroupData } from 'app/data/firebase.interfaces'
 import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox'
 import { MatIconModule } from '@angular/material/icon'
 import { FocusInputComponent } from '../focus-input/focus-input.component'
 import { GroupSelected } from './group.interface'
 import { CdkDragHandle } from '@angular/cdk/drag-drop'
 import { MatIconButton } from '@angular/material/button'
+import { Nullable } from '../../shared/common.interfaces'
 
 @Component({
   selector: 'app-group',
@@ -18,6 +19,8 @@ import { MatIconButton } from '@angular/material/button'
 export class GroupComponent {
   data = input.required<GroupData>()
   selected = input<boolean>()
+
+  changed = output<GroupChanges>()
   selectedChange = output<GroupSelected>()
 
   groupSelected($event:MatCheckboxChange) {
@@ -25,5 +28,15 @@ export class GroupComponent {
       UUID: this.data().UUID,
       isSelected: $event.checked
     })
+  }
+
+  groupLabelChanged($event: Nullable<string>) {
+    if($event) {
+      this.changed.emit({
+        ...this.data(),
+        label: $event,
+        crud: 'update'
+      })
+    }
   }
 }
