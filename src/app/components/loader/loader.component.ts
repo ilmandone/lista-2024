33bce9662f417b549915c742b66ce194eee796d6
Andrea Mandini <a.mandini@mauti.it>
2024-08-30 15:00:10 +0200

    feat(loader): update loader background

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -1,18 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { Nullable } from '../../shared/common.interfaces'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-loader',
   standalone: true,
-  imports: [MatProgressSpinnerModule],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss'
 })
 export class LoaderComponent {
-  bgTransparent = input<boolean>(true);
+  bg = input<Nullable<string>>(null);
   diameter = input<number>(30)
   loading = input.required<boolean>()
   mode = input.required<'determinate' | 'indeterminate'>()
   strokeWidth = input<number>(3)
   value = input<number>(0)
+
+  test = computed(() => console.log(this.bg()))
 }
