91dc5ae802170ee1150f3f2e8a0dad463d0fa53a
Andrea Mandini <a.mandini@vargroup.it>
2024-09-09 11:25:37 +0200

    feat(loader):  color input

--- src/app/components/loader/loader.component.ts
+++ src/app/components/loader/loader.component.ts
@@ -2,16 +2,18 @@ import { Component, computed, input } from '@angular/core'
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
 import { Nullable } from '../../shared/common.interfaces'
 import { CommonModule } from '@angular/common'
+import { SpinnerColorDirective } from './directive/spinner-color.directive'
 
 @Component({
   selector: 'app-loader',
   standalone: true,
-  imports: [CommonModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatProgressSpinnerModule, SpinnerColorDirective],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss'
 })
 export class LoaderComponent {
   bg = input<Nullable<string>>(null)
+  color = input<Nullable<string>>(null)
   diameter = input<number>(30)
   loading = input.required<boolean>()
   mode = input.required<'determinate' | 'indeterminate'>()
