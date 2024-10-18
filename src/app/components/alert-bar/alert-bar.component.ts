717056bb235e33f16c9f3d25677746ec9e3c9b12
Andrea Mandini <a.mandini@mauti.it>
2024-10-18 15:10:27 +0200

    feat(alert-bar): end

--- src/app/components/alert-bar/alert-bar.component.ts
+++ src/app/components/alert-bar/alert-bar.component.ts
@@ -1,12 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, effect, input } from '@angular/core'
+import { Severity } from '../../shared/common.interfaces'
+import { CommonModule } from '@angular/common'
+
 
 @Component({
   selector: 'app-alert-bar',
   standalone: true,
-  imports: [],
-  templateUrl: './alert-bar.component.html',
-  styleUrl: './alert-bar.component.scss'
+  imports: [CommonModule],
+  styleUrl: 'alert-bar.component.scss',
+  template: `
+    @if (show) {
+      <div
+        class="alert-bar"
+        [ngClass]="severity().level"
+      ></div>
+    }
+  `
 })
 export class AlertBarComponent {
 
+  private readonly ALERT_TIMEOUT = 1500
+
+  severity = input.required<{level: Severity | null}>()
+  show = false
+  showTimeoutID: number | null = null
+
+  constructor() {
+    effect(() => {
+      if (this.severity().level) {
+
+        if (this.showTimeoutID)
+          window.clearTimeout(this.showTimeoutID)
+
+        this.show = true
+
+        this.showTimeoutID = window.setTimeout(() => {
+            this._clearTimeout(this.showTimeoutID!)
+            this.show = false
+          },
+          this.ALERT_TIMEOUT)
+      }
+    })
+  }
+
+  private _clearTimeout(id: number) {
+    window.clearTimeout(id)
+    this.showTimeoutID = null
+  }
 }
