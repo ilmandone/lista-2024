c95fcbb4ff45baf50afee84e4bc799cfb060478a
Andrea Mandini <a.mandini@mauti.it>
2024-10-18 11:55:46 +0200

    feat: alert-bar.component creation

---
+++ src/app/components/alert-bar/alert-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert-bar.component.html',
+  styleUrl: './alert-bar.component.scss'
+})
+export class AlertBarComponent {
+
+}
