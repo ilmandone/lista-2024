95cf5c85e897856247c27ef6c5407f2ec52746b6
Andrea Mandini <a.mandini@mauti.it>
2024-12-21 11:23:39 +0100

    feat (group-service): load-group as observable

--- src/app/pages/new-list/new-list.groups.service.ts
+++ src/app/pages/new-list/new-list.groups.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core'
+import { FirebaseService } from '../../data/firebase.service'
+import { GroupData } from '../../data/firebase.interfaces'
+import { from, Observable, map } from 'rxjs'
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class NewListGroupsService {
 
-  constructor() { }
+  private _firebaseSrv = inject(FirebaseService)
+
+  /**
+   * Return an observable of Record /
+   * @param useCache
+   */
+  loadGroups(useCache = false): Observable<Record<string,  GroupData>> {
+    return from(this._firebaseSrv.loadGroups(useCache)).pipe(
+      map(r => r.reduce(
+        (acc: Record<string, GroupData>, val) => {
+          acc[val.UUID] = val
+          return acc
+        }, {}))
+    )
+  }
 }
